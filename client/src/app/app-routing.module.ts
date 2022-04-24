3e759d148644b8b247b856d47bde45e7aafbfcb3
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2022-04-24 13:51:58 +0000

    grouping pages into pages folder

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,32 +4,32 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Routes components
   // Auth
-    import { LoginComponent } from './auth/login/login.component';
+    import { LoginComponent } from './pages/auth/login/login.component';
   // activities
-    import { ActivitiesAllComponent } from './activities/activities-all/activities-all.component';
-    import { ActivitiesAddComponent } from './activities/activities-add/activities-add.component';
-    import { ActivitiesUpdateComponent } from './activities/activities-update/activities-update.component';
-    import { ActivitiesParticipateComponent } from './activities/activities-participate/activities-participate.component';
+    import { ActivitiesAllComponent } from './pages/activities/activities-all/activities-all.component';
+    import { ActivitiesAddComponent } from './pages/activities/activities-add/activities-add.component';
+    import { ActivitiesUpdateComponent } from './pages/activities/activities-update/activities-update.component';
+    import { ActivitiesParticipateComponent } from './pages/activities/activities-participate/activities-participate.component';
   // admin
-    import { AdminsAllComponent } from './admin/admins-all/admins-all.component';
-    import { AdminsAddComponent } from './admin/admins-add/admins-add.component';
-    import { AdminsUpdateComponent } from './admin/admins-update/admins-update.component';
+    import { AdminsAllComponent } from './pages/admin/admins-all/admins-all.component';
+    import { AdminsAddComponent } from './pages/admin/admins-add/admins-add.component';
+    import { AdminsUpdateComponent } from './pages/admin/admins-update/admins-update.component';
   // Attendance
-    import { AttendanceAllComponent } from './attendance/attendance-all/attendance-all.component';
-    import { AttendanceAddComponent } from './attendance/attendance-add/attendance-add.component';
-    import { AttendanceUpdateComponent } from './attendance/attendance-update/attendance-update.component';
+    import { AttendanceAllComponent } from './pages/attendance/attendance-all/attendance-all.component';
+    import { AttendanceAddComponent } from './pages/attendance/attendance-add/attendance-add.component';
+    import { AttendanceUpdateComponent } from './pages/attendance/attendance-update/attendance-update.component';
   // Exercise
-    import { ExerciseAllComponent } from './exercise/exercise-all/exercise-all.component';
-    import { ExerciseAddComponent } from './exercise/exercise-add/exercise-add.component';
-    import { ExerciseUpdateComponent } from './exercise/exercise-update/exercise-update.component';
+    import { ExerciseAllComponent } from './pages/exercise/exercise-all/exercise-all.component';
+    import { ExerciseAddComponent } from './pages/exercise/exercise-add/exercise-add.component';
+    import { ExerciseUpdateComponent } from './pages/exercise/exercise-update/exercise-update.component';
   // Participant
-    import { ParticipantAllComponent } from './participant/participant-all/participant-all.component';
-    import { ParticipantAddComponent } from './participant/participant-add/participant-add.component';
-    import { ParticipantUpdateComponent } from './participant/participant-update/participant-update.component';
+    import { ParticipantAllComponent } from './pages/participant/participant-all/participant-all.component';
+    import { ParticipantAddComponent } from './pages/participant/participant-add/participant-add.component';
+    import { ParticipantUpdateComponent } from './pages/participant/participant-update/participant-update.component';
   // Responsible
-    import { ResponsibleAllComponent } from './responsible/responsible-all/responsible-all.component';
-    import { ResponsibleAddComponent } from './responsible/responsible-add/responsible-add.component';
-    import { ResponsibleUpdateComponent } from './responsible/responsible-update/responsible-update.component';
+    import { ResponsibleAllComponent } from './pages/responsible/responsible-all/responsible-all.component';
+    import { ResponsibleAddComponent } from './pages/responsible/responsible-add/responsible-add.component';
+    import { ResponsibleUpdateComponent } from './pages/responsible/responsible-update/responsible-update.component';
 
 const routes: Routes = [
   // Auth
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,26 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ActivitiesAllComponent } from './activities/activities-all/activities-all.component';
-import { ActivitiesAddComponent } from './activities/activities-add/activities-add.component';
-import { ActivitiesUpdateComponent } from './activities/activities-update/activities-update.component';
-import { ActivitiesParticipateComponent } from './activities/activities-participate/activities-participate.component';
-import { AdminsAllComponent } from './admin/admins-all/admins-all.component';
-import { AdminsAddComponent } from './admin/admins-add/admins-add.component';
-import { AdminsUpdateComponent } from './admin/admins-update/admins-update.component';
-import { AttendanceAllComponent } from './attendance/attendance-all/attendance-all.component';
-import { AttendanceAddComponent } from './attendance/attendance-add/attendance-add.component';
-import { AttendanceUpdateComponent } from './attendance/attendance-update/attendance-update.component';
-import { ExerciseAllComponent } from './exercise/exercise-all/exercise-all.component';
-import { ExerciseAddComponent } from './exercise/exercise-add/exercise-add.component';
-import { ExerciseUpdateComponent } from './exercise/exercise-update/exercise-update.component';
-import { ParticipantAllComponent } from './participant/participant-all/participant-all.component';
-import { ParticipantUpdateComponent } from './participant/participant-update/participant-update.component';
-import { ParticipantAddComponent } from './participant/participant-add/participant-add.component';
-import { ResponsibleAllComponent } from './responsible/responsible-all/responsible-all.component';
-import { ResponsibleAddComponent } from './responsible/responsible-add/responsible-add.component';
-import { ResponsibleUpdateComponent } from './responsible/responsible-update/responsible-update.component';
-import { LoginComponent } from './auth/login/login.component';
+import { ActivitiesAllComponent } from './pages/activities/activities-all/activities-all.component';
+import { ActivitiesAddComponent } from './pages/activities/activities-add/activities-add.component';
+import { ActivitiesUpdateComponent } from './pages/activities/activities-update/activities-update.component';
+import { ActivitiesParticipateComponent } from './pages/activities/activities-participate/activities-participate.component';
+import { AdminsAllComponent } from './pages/admin/admins-all/admins-all.component';
+import { AdminsAddComponent } from './pages/admin/admins-add/admins-add.component';
+import { AdminsUpdateComponent } from './pages/admin/admins-update/admins-update.component';
+import { AttendanceAllComponent } from './pages/attendance/attendance-all/attendance-all.component';
+import { AttendanceAddComponent } from './pages/attendance/attendance-add/attendance-add.component';
+import { AttendanceUpdateComponent } from './pages/attendance/attendance-update/attendance-update.component';
+import { ExerciseAllComponent } from './pages/exercise/exercise-all/exercise-all.component';
+import { ExerciseAddComponent } from './pages/exercise/exercise-add/exercise-add.component';
+import { ExerciseUpdateComponent } from './pages/exercise/exercise-update/exercise-update.component';
+import { ParticipantAllComponent } from './pages/participant/participant-all/participant-all.component';
+import { ParticipantUpdateComponent } from './pages/participant/participant-update/participant-update.component';
+import { ParticipantAddComponent } from './pages/participant/participant-add/participant-add.component';
+import { ResponsibleAllComponent } from './pages/responsible/responsible-all/responsible-all.component';
+import { ResponsibleAddComponent } from './pages/responsible/responsible-add/responsible-add.component';
+import { ResponsibleUpdateComponent } from './pages/responsible/responsible-update/responsible-update.component';
+import { LoginComponent } from './pages/auth/login/login.component';
 
 @NgModule({
   declarations: [
